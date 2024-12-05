f8ced59acd3137cd88406b9f6db31aaf8289ff70
Kaic Machado <kaicmachado2@gmail.com>
2024-12-05 11:03:33 -0300

    adicionado card de unha em gel

--- src/components/Servicos.jsx
+++ src/components/Servicos.jsx
@@ -2,6 +2,7 @@ import CardServico from "./CardServico";
 import ManicureImg from "../assets/manicure.png";
 import PedicureImg from "../assets/pedicure.png";
 import PlasticaImg from "../assets/plastica.png";
+import UnhaGelImg from "../assets/unhagel.png";
 import styles from "./CardServico.module.css";
 const Servicos = () => {
  return (
@@ -9,6 +10,7 @@ const Servicos = () => {
    <ul className={styles.lista}>
     <CardServico title="Manicure" img={ManicureImg} />
     <CardServico title="Pedicure" img={PedicureImg} />
+    <CardServico title="Unhas em Gel" img={UnhaGelImg} />
     <CardServico title="Plástica dos Pés" img={PlasticaImg} />
    </ul>
 
